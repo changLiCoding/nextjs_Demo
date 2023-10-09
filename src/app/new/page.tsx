c92f1a6e8f28c19c8793512c004bf80ad6dc51f5
javascriptsucks <evix1988@live.cn>
2023-10-09 01:03:42 -0600

    create new todo in use server

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,64 @@
+import { prisma } from "@/db";
+import createTodo from "@/utils/createTodo";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+async function handleSubmit(data: FormData) {
+	"use server";
+
+	const title = data.get("title")?.valueOf();
+
+	if (typeof title !== "string" || title.length === 0) {
+		throw new Error("Title is required");
+	}
+
+	await prisma.todo.create({
+		data: { title, completed: false },
+	});
+	console.log("Hello World");
+	redirect("/");
+}
+
 export default function New() {
 	return (
-		<div>
-			<h3>New pages</h3>
-		</div>
+		<>
+			<header className='flex justify-between items-center mb-4'>
+				<Link href={"/"}>
+					<h1 className='text-2xl hover:bg-slate-700 rounded-xl px-3 py-1'>
+						Todos
+					</h1>
+				</Link>
+				<Link
+					href='/'
+					className='border border-slate-300 text-slate-300 px-2 py-1 rounded-xl hover:bg-slate-700 focus-within:bg-slate-700 outline-none'>
+					Go Home
+				</Link>
+			</header>
+			<div>
+				<h3>Create Todo</h3>
+				<form
+					action={handleSubmit}
+					className='flex gap-2 flex-col'>
+					<input
+						type='text'
+						name='title'
+						placeholder='Title'
+						className='border border-slate-300 bg-transparent rounded-lg px-2 py-1 outline-none focus-within:border-slate-100'
+					/>
+					<div className='flex justify-end gap-8 mt-4'>
+						<Link
+							className='border border-slate-300 text-slate-300 px-2 py-1 rounded-xl hover:bg-slate-700 focus-within:bg-slate-700 outline-none'
+							href='..'>
+							Cancel
+						</Link>
+						<button
+							className='border border-slate-300 text-slate-300 px-2 py-1 rounded-xl hover:bg-slate-700 focus-within:bg-slate-700 outline-none'
+							type='submit'>
+							Submit
+						</button>
+					</div>
+				</form>
+			</div>
+		</>
 	);
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -18,10 +18,10 @@ export default function TodoItem({ title, completed, id }: TodoItemProps) {
 			/>
 			<label
 				htmlFor={id}
-				className='peer-checked:line-through'>
+				className='peer-checked:line-through peer-checked:text-slate-500'>
 				{title}{" "}
 			</label>
-			<span className='text-sm text-slate-300'>
+			<span className='text-sm text-slate-300 peer-checked:line-through peer-checked:text-slate-500'>
 				The {title} is {completed ? "completed" : "not completed"}
 			</span>
 		</li>
